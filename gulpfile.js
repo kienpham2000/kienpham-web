78aa21f9be51c284d6e793995afbc5cd8d0a2103
Kien Pham <shop@kienpham.com>
2016-06-15 12:18:52 -0700

    min

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var jsmin = require('gulp-jsmin');
+var concat = require('gulp-concat');
 
 gulp.task('default', function() {
-    gulp.src('js/*.js')
+    gulp.src(['js/*.js', '!js/*.min.js'])
+    .pipe(concat('main.js'))
     .pipe(jsmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('dist'));
 
-  return gulp.src('css/*.css')
+  return gulp.src(['css/*.css', '!css/*.min.css'])
+    .pipe(concat('main.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('dist'));
 });
